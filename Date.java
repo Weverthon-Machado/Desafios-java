fe2079d223fed37bc0464434ef39d946eb1b02a2
WEVERTHON VAZ <112637629+Weverthon-Machado@users.noreply.github.com>
2025-04-10 15:13:26 -0300

    DesafioDate

---
+++ Date.java
@@ -0,0 +1,5 @@
+public class Date {
+    int dia = 1;
+    int mes = 12;
+    int ano = 2002;
+}
---
+++ DesafioDate.java
@@ -0,0 +1,12 @@
+public class DesafioDate {
+    public static void main(String[] args) {
+
+        Date d1 = new Date();
+        d1.ano = 7;
+        d1.ano = 12;
+        d1.ano = 2002;
+
+        System.out.println(d1);
+
+    }
+}
